46f15590bdd4b44d03676abaf551d9e41158aa80
Garrigan Stafford <matorin57@gmail.com>
2026-07-08 10:27:02 -0500

    typo

--- tools/utils.py
+++ tools/utils.py
@@ -1,8 +1,8 @@
 
 DATE_TIME_FORMAT = "%Y-%m-%d %H:%M %Z"
 
-def getTimeZoneNameFromDatetime(datetime) -> str:
-    info = startTime.tzinfo
+def getTimeZoneNameFromDatetime(d) -> str:
+    info = d.tzinfo
     if info is None:
         return None
     if hasattr(info, "zone") and info.zone is not None:
@@ -10,5 +10,5 @@ def getTimeZoneNameFromDatetime(datetime) -> str:
     elif hasattr(info, "key") and info.key is not None:
         return info.key
     raise Exception("Timezone object has neither key nor zone")
-    
+
     
