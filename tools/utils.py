dd77cfd6bd0b4e98b483c532a7400befa2bf1e88
Garrigan Stafford <matorin57@gmail.com>
2026-07-08 10:42:26 -0500

    Uglier timezone fix

--- tools/utils.py
+++ tools/utils.py
@@ -1,3 +1,6 @@
+import zoneinfo
+import datetime
+from .EventAutomation import ActionNetworkAutomation
 
 DATE_TIME_FORMAT = "%Y-%m-%d %H:%M %Z"
 
@@ -9,6 +12,17 @@ def getTimeZoneNameFromDatetime(d) -> str:
         return info.zone
     elif hasattr(info, "key") and info.key is not None:
         return info.key
-    raise Exception("Timezone object has neither key nor zone")
+    # Ugly hack but will work for now have issue to fix this whole timezone debacle better https://github.com/Austin-DSA/GeneralToolsWebsite/issues/26
+    acceptedTimeZones = ActionNetworkAutomation.TimeZone.TZ_TO_AN_TZ.keys()
+
+    target_offset = info.utcoffset()
+    utc_instant = d.astimezone(datetime.timezone.utc)
+
+    for zone in acceptedTimeZones:
+        zi = zoneinfo.ZoneInfo(zone)
+        if utc_instant.astimezone(zi).utcoffset() == target_offset:
+            return zone
+    
+    raise Exception(f"Unkown UTC offset {target_offset} for datetime {d}")
 
     
